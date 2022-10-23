c44b94804c05ac3e5e3a666772e18f3d3ea1206c
Alex Goldstein <Alex_Goldstein@outlook.com>
2022-10-23 18:16:35 -0400

    SecondTestCase

--- Linked List Implementation/src/AppTest.java
+++ Linked List Implementation/src/AppTest.java
@@ -1,7 +1,9 @@
 import static org.junit.Assert.assertEquals;
-import java.util.Iterator;
 
 import org.junit.Test;
+
+import java.util.Iterator;
+import java.lang.Math;
 
 public class AppTest{
     @Test
@@ -13,10 +15,79 @@ public class AppTest{
             reverseList.append((char)(90-i));
         }
         originList.reverse();
-        Iterator<Character> originIter = originList.iterator();
-        Iterator<Character> reverseIter = reverseList.iterator();
+        compare(originList, reverseList);
+    }
+
+    @Test
+    public void testRemoveArbitraryLocation(){
+        int randomPos = (int) (Math.random()*26);
+        DoubleList<Character> userList = new DoubleList<Character>();
+        DoubleList<Character> compareList = new DoubleList<Character>();
+        for(int i = 0; i < 26; i++){
+            userList.append((char) (i+65));
+            if(i != randomPos){
+                compareList.append((char) (i+65));
+            }
+        }
+        userList.remove(randomPos);
+        compare(userList, compareList);
+    }
+
+    @Test
+    public void testAddArbitraryLocation(){
+
+    }
+
+    @Test
+    public void testAllLettersLoop(){
+
+    }
+
+    @Test
+    public void testAddAllLetters(){
+
+    }
+
+    @Test
+    public void testEmptySizeZero(){
+
+    }
+
+    @Test
+    public void testAppendAfterIteratorRemove(){
+
+    }
+
+    @Test
+    public void testNextAfterEnd(){
+
+    }
+
+    @Test
+    public void testRemoveWithoutNextThrowsException(){
+
+    }
+
+    @Test
+    public void testRemoveLessThanM(){
+
+    }
+
+    @Test
+    public void testIteratorRemoveAll(){
+
+    }
+
+    @Test
+    public void testAddAllLettersForEach(){
+
+    }
+
+    private void compare(DoubleList<Character> UserList, DoubleList<Character> CompareList){
+        Iterator<Character> originIter = UserList.iterator();
+        Iterator<Character> compareIter = CompareList.iterator();
         while(originIter.hasNext()){
-            assertEquals(originIter.next(), reverseIter.next());
+            assertEquals(originIter.next(), compareIter.next());
         }
     }
 }
